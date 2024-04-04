7a733cd9473bcaa22315d42dc2a9db39deb85c18
aelfk <aelfky66@gmail.com>
2024-04-04 05:16:19 +0200

    second

---
+++ lib/home/data/models/myGallery_model/myGallery_model.dart
@@ -0,0 +1,43 @@
+class GalleryModel {
+  String? status;
+  Data? data;
+  String? message;
+
+  GalleryModel({this.status, this.data, this.message});
+
+  GalleryModel.fromJson(Map<String, dynamic> json) {
+    status = json['status'];
+    if (json['data'] != null) {
+      data = Data.fromJson(json['data']);
+    } else {
+      data = null;
+    }
+    message = json['message'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['status'] = status;
+    if (this.data != null) {
+      data['data'] = this.data!.toJson();
+    }
+    data['message'] = message;
+    return data;
+  }
+}
+
+class Data {
+  List<String>? images;
+
+  Data({this.images});
+
+  Data.fromJson(Map<String, dynamic> json) {
+    images = json['images'].cast<String>();
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['images'] = images;
+    return data;
+  }
+}
